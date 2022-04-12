93dcadd73249eed69b3f0a3c727527d9cf185142
Larissa_Gomes <88160134+LarissaGomes333@users.noreply.github.com>
2022-04-12 07:59:52 -0300

    Add files via upload

---
+++ build/precache-manifest.c27d2e3dee0aedb71727724c45fd12ec.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c923dcec49b12de2988",
+    "url": "/static/css/main.3a65330c.chunk.css"
+  },
+  {
+    "revision": "8c923dcec49b12de2988",
+    "url": "/static/js/main.8c923dce.chunk.js"
+  },
+  {
+    "revision": "05a8e767b6b683ee5dc9",
+    "url": "/static/js/1.05a8e767.chunk.js"
+  },
+  {
+    "revision": "6a9b5a9b8cb4b0e95abb",
+    "url": "/static/js/2.6a9b5a9b.chunk.js"
+  },
+  {
+    "revision": "ed64082d111baeb537c6",
+    "url": "/static/js/runtime~main.ed64082d.js"
+  },
+  {
+    "revision": "8371f05f043c719152cacab7fd617e87",
+    "url": "/static/media/Logo.8371f05f.png"
+  },
+  {
+    "revision": "ae6a13351de4d6505074ff4b11ac0dbd",
+    "url": "/static/media/Celular.ae6a1335.png"
+  },
+  {
+    "revision": "19773998c9aa6048bfc28920fec8f87d",
+    "url": "/static/media/imagem1.19773998.png"
+  },
+  {
+    "revision": "36c03ee0a5022c84b807571767e8a993",
+    "url": "/static/media/imagem2.36c03ee0.png"
+  },
+  {
+    "revision": "093fcf715937b1fca70d9db2c5b0873f",
+    "url": "/static/media/imagem3.093fcf71.png"
+  },
+  {
+    "revision": "4e1e0887c8d966d7fb68758aedfeaeaa",
+    "url": "/static/media/imagem4.4e1e0887.png"
+  },
+  {
+    "revision": "a75048865651f9d5b32839f8db5b0067",
+    "url": "/static/media/appstore.a7504886.png"
+  },
+  {
+    "revision": "59a3df4260f863fd8f0c4bc0f2cb7e5c",
+    "url": "/index.html"
+  }
+];
